8a9051d72b1ceb994b416f3233fc57d8dabc45be
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-03-19 18:03:24 +0900

    Add scores from PPO paper ACER algo

---
+++ sotarl/db/paper__proximal_policy_optimization_algorithms/algo__acer/__init__.py
@@ -0,0 +1,15 @@
+from .entries import entries
+
+
+# Specify ALGORITHM
+algo = {
+    # ALGORITHM
+    "algo-title": "Actor-Critic with Experience Replay",
+    "algo-nickname": "ACER",
+
+    # HYPERPARAMETERS
+    "algo-frames": 40000000,
+}
+
+# Populate entries
+entries = [{**entry, **algo} for entry in entries]
