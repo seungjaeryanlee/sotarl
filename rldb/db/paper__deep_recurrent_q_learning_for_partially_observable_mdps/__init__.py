8a82f9b1a37bef7df2b828a777205c3dd337dbe8
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-03-26 06:18:43 +0000

    Remove DRQN paper DQN2015 algo

--- rldb/db/paper__deep_recurrent_q_learning_for_partially_observable_mdps/__init__.py
+++ rldb/db/paper__deep_recurrent_q_learning_for_partially_observable_mdps/__init__.py
@@ -1,4 +1,3 @@
-from .algo__dqn2015 import entries as dqn2015_entries
 from .algo__dqn2015_ours import entries as dqn2015_ours_entries
 from .algo__drqn import entries as drqn_entries
 
@@ -44,4 +43,4 @@ entries = (
 )
 entries = [{**entry, **source} for entry in entries]
 
-assert len(entries) == 9 * 3
+assert len(entries) == 9 * 2
