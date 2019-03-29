d6f39277d55f1e8698870a0efc2df35861d60743
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-03-30 05:28:08 +0900

    Populate Rainbow paper DQN algo

--- rldb/db/paper__rainbow/algo__dqn/entries.py
+++ rldb/db/paper__rainbow/algo__dqn/entries.py
@@ -2,42 +2,42 @@ human_entries = [
     {
         'env-title': 'atari-berzerk',
         'env-variant': 'Human start',
-        'score': ,
+        'score': 493.4,
     },
     {
         'env-title': 'atari-defender',
         'env-variant': 'Human start',
-        'score': ,
+        'score': 15917.5,
     },
     {
         'env-title': 'atari-phoenix',
         'env-variant': 'Human start',
-        'score': ,
+        'score': 7484.8,
     },
     {
         'env-title': 'atari-pitfall',
         'env-variant': 'Human start',
-        'score': ,
+        'score': -113.2,
     },
     {
         'env-title': 'atari-skiing',
         'env-variant': 'Human start',
-        'score': ,
+        'score': -12142.1,
     },
     {
         'env-title': 'atari-solaris',
         'env-variant': 'Human start',
-        'score': ,
+        'score': 1295.4,
     },
     {
         'env-title': 'atari-surround',
         'env-variant': 'Human start',
-        'score': ,
+        'score': -6.0,
     },
     {
         'env-title': 'atari-yars-revenge',
         'env-variant': 'Human start',
-        'score': ,
+        'score': 4577.5,
     },
 ]
 
@@ -45,42 +45,42 @@ noop_entries = [
     {
         'env-title': 'atari-berzerk',
         'env-variant': 'No-op start',
-        'score': ,
+        'score': 585.6,
     },
     {
         'env-title': 'atari-defender',
         'env-variant': 'No-op start',
-        'score': ,
+        'score': 23633.0,
     },
     {
         'env-title': 'atari-phoenix',
         'env-variant': 'No-op start',
-        'score': ,
+        'score': 8485.2,
     },
     {
         'env-title': 'atari-pitfall',
         'env-variant': 'No-op start',
-        'score': ,
+        'score': -286.1,
     },
     {
         'env-title': 'atari-skiing',
         'env-variant': 'No-op start',
-        'score': ,
+        'score': -13062.3,
     },
     {
         'env-title': 'atari-solaris',
         'env-variant': 'No-op start',
-        'score': ,
+        'score': 3482.8,
     },
     {
         'env-title': 'atari-surround',
         'env-variant': 'No-op start',
-        'score': ,
+        'score': -5.6,
     },
     {
         'env-title': 'atari-yars-revenge',
         'env-variant': 'No-op start',
-        'score': ,
+        'score': 18089.9,
     },
 ]
 
