3da2eda2d28cefc5064433ae3fb24dc245259ab7
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2019-03-30 12:12:16 +0900

    Add ACKTR paper A2C algo

--- rldb/db/paper__acktr/algo__a2c/entries.py
+++ rldb/db/paper__acktr/algo__a2c/entries.py
@@ -2,31 +2,31 @@ entries = [
     {
         'env-title': 'atari-beam-rider',
         'env-variant': 'No-op start',
-        'score': ,
+        'score': 8148.1,
     },
     {
         'env-title': 'atari-breakout',
         'env-variant': 'No-op start',
-        'score': ,
+        'score': 581.6,
     },
     {
         'env-title': 'atari-pong',
         'env-variant': 'No-op start',
-        'score': ,
+        'score': 19.9,
     },
     {
         'env-title': 'atari-qbert',
         'env-variant': 'No-op start',
-        'score': ,
+        'score': 15967.4,
     },
     {
         'env-title': 'atari-seaquest',
         'env-variant': 'No-op start',
-        'score': ,
+        'score': 1754.0,
     },
     {
         'env-title': 'atari-space-invaders',
         'env-variant': 'No-op start',
-        'score': ,
+        'score': 1757.2,
     },
 ]
