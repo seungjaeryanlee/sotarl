6734a0c98b3c290d531de79ac8a3ef676bf9ad78
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2019-03-30 13:30:01 +0900

    Add ACKTR paper TRPO algo

--- rldb/db/paper__acktr/algo__trpo/entries.py
+++ rldb/db/paper__acktr/algo__trpo/entries.py
@@ -2,31 +2,31 @@ entries = [
     {
         'env-title': 'atari-beam-rider',
         'env-variant': 'No-op start',
-        'score': ,
+        'score': 670.0,
     },
     {
         'env-title': 'atari-breakout',
         'env-variant': 'No-op start',
-        'score': ,
+        'score': 14.7,
     },
     {
         'env-title': 'atari-pong',
         'env-variant': 'No-op start',
-        'score': ,
+        'score': -1.2,
     },
     {
         'env-title': 'atari-qbert',
         'env-variant': 'No-op start',
-        'score': ,
+        'score': 971.8,
     },
     {
         'env-title': 'atari-seaquest',
         'env-variant': 'No-op start',
-        'score': ,
+        'score': 810.4,
     },
     {
         'env-title': 'atari-space-invaders',
         'env-variant': 'No-op start',
-        'score': ,
+        'score': 465.1,
     },
 ]
