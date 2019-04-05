7159930259e84da969e6c4515df0397f0199dc57
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-04-06 01:56:15 +0900

    Add unit tests for filter match

--- tests/test_find.py
+++ tests/test_find.py
@@ -28,3 +28,26 @@ def test_find_all_return_empty_list():
     all_entries = rldb.find_all({ 'env-title': 'There is no env with this title.' })
 
     assert all_entries == []
+
+
+def test_find_one_return_match_all_filter():
+    """Make sure that entry returned by find_one() matches all filters."""
+    entry = rldb.find_one({
+        'algo-nickname': 'DQN',
+        'env-title': 'atari-pong',
+    })
+
+    assert entry['algo-nickname'] == 'DQN'
+    assert entry['env-title'] == 'atari-pong'
+
+
+def test_find_all_return_match_all_filter():
+    """Make sure that all entries returned by find_all() matches all filters."""
+    all_entries = rldb.find_all({
+        'algo-nickname': 'DQN',
+        'env-title': 'atari-pong',
+    })
+
+    for entry in all_entries:
+        assert entry['algo-nickname'] == 'DQN'
+        assert entry['env-title'] == 'atari-pong'
