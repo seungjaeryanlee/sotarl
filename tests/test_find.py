b85c0973a0a028dc75758af3916eba693fd0bd0f
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-04-06 01:47:13 +0900

    Add unit tests for impossible filters

--- tests/test_find.py
+++ tests/test_find.py
@@ -4,9 +4,9 @@ import rldb
 
 def test_find_one_rtype_dict():
     """Verify that find_one() returns an entry (dict), not a list of entries."""
-    all_entries = rldb.find_one({})
+    entry = rldb.find_one({})
 
-    assert type(all_entries) == dict
+    assert type(entry) == dict
 
 
 def test_find_all_rtype_list():
@@ -16,3 +16,15 @@ def test_find_all_rtype_list():
     assert type(all_entries) == list
 
 
+def test_find_one_return_none():
+    """Verify that find_one() returns None if nothing is found."""
+    entry = rldb.find_one({ 'env-title': 'There is no env with this title.' })
+
+    assert entry is None
+
+
+def test_find_all_return_empty_list():
+    """Verify that find_all() returns an empty list if nothing is found."""
+    all_entries = rldb.find_all({ 'env-title': 'There is no env with this title.' })
+
+    assert all_entries == []
