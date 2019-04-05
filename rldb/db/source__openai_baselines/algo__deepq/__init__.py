f0fe0c93f695791b3f0a66dbe2b61e804401d796
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-04-05 17:04:08 +0900

    Add OpenAI Baselines DQN algo

---
+++ rldb/db/source__openai_baselines/algo__deepq/__init__.py
@@ -0,0 +1,23 @@
+"""
+DeepQ scores from OpenAI Baselines.
+
+ 7 entries
+------------------------------------------------------------------------
+ 7 unique entries
+
+"""
+from .entries import entries
+
+
+# Specify ALGORITHM
+algo = {
+    # ALGORITHM
+    "algo-title": "Deep Q-Network",
+    "algo-nickname": "DQN",
+    "algo-source-title": "Human-level Control through Deep Reinforcement Learning",
+}
+
+# Populate entries
+entries = [{**entry, **algo} for entry in entries]
+
+assert len(entries) == 7
