d73c71eae94e5aad48bd50ec82cd4262a6291dbe
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-04-23 17:33:00 +0900

    Add DMLab-30 Random and Human

---
+++ rldb/db/paper__impala/algo__dmlab_30_random/entries.py
@@ -0,0 +1,152 @@
+entries = [
+    {
+        "env-title": "rooms_collect_good_objects_test",
+        "env-variant": "No-op start",
+        "score": 0.1,
+    },
+    {
+        "env-title": "rooms_exploit_deferred_effects_test",
+        "env-variant": "No-op start",
+        "score": 8.5,
+    },
+    {
+        "env-title": "rooms_select_nonmatching_object",
+        "env-variant": "No-op start",
+        "score": 0.3,
+    },
+    {
+        "env-title": "rooms_watermaze",
+        "env-variant": "No-op start",
+        "score": 4.1,
+    },
+    {
+        "env-title": "rooms_keys_doors_puzzle",
+        "env-variant": "No-op start",
+        "score": 4.1,
+    },
+    {
+        "env-title": "language_select_described_object",
+        "env-variant": "No-op start",
+        "score": -0.1,
+    },
+    {
+        "env-title": "language_select_located_object",
+        "env-variant": "No-op start",
+        "score": 1.9,
+    },
+    {
+        "env-title": "language_execute_random_task",
+        "env-variant": "No-op start",
+        "score": -5.9,
+    },
+    {
+        "env-title": "language_answer_quantitative_question",
+        "env-variant": "No-op start",
+        "score": -0.3,
+    },
+    {
+        "env-title": "lasertag_one_opponent_large",
+        "env-variant": "No-op start",
+        "score": -0.2,
+    },
+    {
+        "env-title": "lasertag_three_oponents_large",
+        "env-variant": "No-op start",
+        "score": -0.2,
+    },
+    {
+        "env-title": "lasertag_one_opponent_small",
+        "env-variant": "No-op start",
+        "score": -0.1,
+    },
+    {
+        "env-title": "lasertag_three_opponents_small",
+        "env-variant": "No-op start",
+        "score": -0.1,
+    },
+    {
+        "env-title": "natlab_fixed_large_map",
+        "env-variant": "No-op start",
+        "score": 2.2,
+    },
+    {
+        "env-title": "natlab_varying_map_regrowth",
+        "env-variant": "No-op start",
+        "score": 3.0,
+    },
+    {
+        "env-title": "natlab_varying_map_randomized",
+        "env-variant": "No-op start",
+        "score": 7.3,
+    },
+    {
+        "env-title": "skymaze_irreversible_path_hard",
+        "env-variant": "No-op start",
+        "score": 0.1,
+    },
+    {
+        "env-title": "skymaze_irreversible_path_varied",
+        "env-variant": "No-op start",
+        "score": 14.4,
+    },
+    {
+        "env-title": "pyschlab_arbitrary_visuomotor_mapping",
+        "env-variant": "No-op start",
+        "score": 0.2,
+    },
+    {
+        "env-title": "pyschlab_continuous_recognition",
+        "env-variant": "No-op start",
+        "score": 0.2,
+    },
+    {
+        "env-title": "pyschlab_sequential_comparison",
+        "env-variant": "No-op start",
+        "score": 0.1,
+    },
+    {
+        "env-title": "pyschlab_visual_search",
+        "env-variant": "No-op start",
+        "score": 0.1,
+    },
+    {
+        "env-title": "explore_object_locations_small",
+        "env-variant": "No-op start",
+        "score": 3.6,
+    },
+    {
+        "env-title": "explore_object_locations_large",
+        "env-variant": "No-op start",
+        "score": 4.7,
+    },
+    {
+        "env-title": "explore_obstructed_goals_small",
+        "env-variant": "No-op start",
+        "score": 6.8,
+    },
+    {
+        "env-title": "explore_obstructed_goals_large",
+        "env-variant": "No-op start",
+        "score": 2.6,
+    },
+    {
+        "env-title": "explore_goal_locations_small",
+        "env-variant": "No-op start",
+        "score": 7.7,
+    },
+    {
+        "env-title": "explore_goal_locations_large",
+        "env-variant": "No-op start",
+        "score": 3.1,
+    },
+    {
+        "env-title": "explore_object_rewards_few",
+        "env-variant": "No-op start",
+        "score": 2.1,
+    },
+    {
+        "env-title": "explore_object_rewards_many",
+        "env-variant": "No-op start",
+        "score": 2.4,
+    },
+]
